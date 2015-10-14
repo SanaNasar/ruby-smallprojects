eff2eb8e9591da660103f62cf57099e08f54db58
SanaNasar <sanasehereen.cse@gmail.com>
2015-10-13 20:21:28 -0700

    adding fizzbuzz.rb

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,6 @@
 # fizzbuzz problem
 # if a number is divisible by 3, return fizz
-# if a number is divisible vy 5, return buzz
+# if a number is divisible by 5, return buzz
 # if a number is divisible by 3 and 5, return fizzbuzz
 
 def fizzbuzz(num)
